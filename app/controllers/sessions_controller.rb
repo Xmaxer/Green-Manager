cb36d3181518ebd7198ed6ececa4e5114f4c90a2
Kevin J <7211424+Xmaxer@users.noreply.github.com>
2018-10-06 21:48:36 +0100

    Added login system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,18 @@
 class SessionsController < ApplicationController
   def new
+    @temp_user = 1
   end
   def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      login @user
+      redirect_to root_url
+    else
+
+    end
   end
   def destroy
+    logout
+    redirect_to root_url
   end
 end
